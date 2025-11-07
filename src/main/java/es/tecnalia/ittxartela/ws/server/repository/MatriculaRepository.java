f36f9d1ccfc4d189a6e82f156e75a8b094e79e7a
Carlotta Orengia <102922877+Jeheiz@users.noreply.github.com>
2025-11-07 14:18:15 +0100

    Update MatriculaRepository.java

--- src/main/java/es/tecnalia/ittxartela/ws/server/repository/MatriculaRepository.java
+++ src/main/java/es/tecnalia/ittxartela/ws/server/repository/MatriculaRepository.java
@@ -1,7 +1,6 @@
 package es.tecnalia.ittxartela.ws.server.repository;
 
 import java.util.Date;
-import java.sql.Timestamp;
 import java.util.List;
 
 import org.springframework.data.jpa.repository.JpaRepository;
@@ -10,11 +9,12 @@ import org.springframework.data.repository.query.Param;
 
 import es.tecnalia.ittxartela.ws.server.model.Matricula;
 
+public interface MatriculaRepository extends JpaRepository<Matricula, String> {
 
     @Query("SELECT m FROM Matricula m " +
-        "WHERE UPPER(m.dni) = UPPER(:dni) " +
-          "AND m.id_status = 1 " +
-          "AND (m.plataforma IN (0, 1, 2) OR m.plataforma IS NULL) " +
+           "WHERE UPPER(m.dni) = UPPER(:dni) " +
+           "AND m.id_status = 1 " +
+           "AND (m.plataforma IN (0, 1, 2) OR m.plataforma IS NULL) " +
            "AND (:tipoCertificacion IS NULL OR m.id_nivel = :tipoCertificacion) " +
            "AND m.fecha_exam <= :fechaLimite")
     List<Matricula> findCertificacionesValidas(
@@ -23,14 +23,17 @@ import es.tecnalia.ittxartela.ws.server.model.Matricula;
             @Param("fechaLimite") Date fechaLimite
     );
 
+    default List<Matricula> obtenerCertificacionesAprobadas(String dni) {
+        return obtenerCertificacionesAprobadas(dni, null, new Date(System.currentTimeMillis()));
+    }
 
+    default List<Matricula> obtenerCertificacionesAprobadas(String dni, Date fechaLimite) {
+        return obtenerCertificacionesAprobadas(dni, null, fechaLimite);
+    }
 
-default List<Matricula> obtenerCertificacionesAprobadas(String dni) {
- return obtenerCertificacionesAprobadas(dni, null, new Date(System.currentTimeMillis()));
-}
-
-default List<Matricula> obtenerCertificacionesAprobadas(String dni, Date fechaLimite) {
- return obtenerCertificacionesAprobadas(dni, null, fechaLimite);
+    default List<Matricula> obtenerCertificacionesAprobadas(String dni, Integer tipoCertificacion, Date fechaLimite) {
+        return findCertificacionesValidas(dni, tipoCertificacion, fechaLimite);
+    }
 }
 
 default List<Matricula> obtenerCertificacionesAprobadas(String dni, Integer tipoCertificacion, Date fechaLimite) {
